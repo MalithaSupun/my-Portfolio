75a0d43a950fcfe20627a3d896a21c2bb2e2f837
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-05-02 14:08:03 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,10 +7,10 @@ import linkedin from './../Assets/socail icon/linkedin -white.png';
 const Home = () => {
   return (
     <>
-    <div className="bg-backgroundone sm:flex text-white grid h-screen ">
-        <div className="block sm:mt-36 sm:ml-36" >
-            <h1 className='lg:text-5xl text-3xl '>Hi,I'm Malitha Supun👋</h1>
-            <p className='mt-4 text-l'>As a full stack developer, I'm dedicated to crafting fast, accessible, visually appealing, and responsive digital experiences. Each project excites me anew, driving me to innovate and create exceptional solutions. With expertise in both front-end and back-end technologies, I strive to exceed expectations and leave a lasting impression.</p>
+    <div className="bg-backgroundone sm:flex text-white grid ">
+        <div className="block xl:mt-36 xl:ml-36 lg:mt-7 lg:ml-12 md:mt-5 md:ml-6 sm:ml-10 sm:mt-12" >
+            <h1 className='text-3xl xl:text-5xl '>Hi,I'm Malitha Supun👋</h1>
+            <p className='xl:mt-4 xl:text-xl md:mt-4 text-[14px] '>As a full stack developer, I'm dedicated to crafting fast, accessible, visually appealing, and responsive digital experiences. Each project excites me anew, driving me to innovate and create exceptional solutions. With expertise in both front-end and back-end technologies, I strive to exceed expectations and leave a lasting impression.</p>
             <div className="mt-4 text-lg">
             <p>📍 Bentota, Sri Lanka</p>
             </div>
@@ -24,7 +24,7 @@ const Home = () => {
             </div>
             <button id='downloadcv' className="mt-10 border-2 rounded-2xl border-white py-1.5 px-2 text-white ">Download CV</button>
         </div>
-        <div className="mt-7 max-w-[628px] ml-40 l">
+        <div className="mt-7 max-w-[628px] lg:ml-40 l md:ml-5">
         <img src={profilePic} alt="Profile Picture"/>
         </div>
     </div>
