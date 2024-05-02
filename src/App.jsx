3f6fd300e2eb0f4b96f00985d2d09239e2f444d6
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-05-03 00:24:07 +0530

    About me responsive finished

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <Navbar />
       <Home />
-      {/* <Aboutme /> */}
+      <Aboutme />
       {/* <Skills /> */}
       {/* <Projects/> */}
       {/* <Getintouch /> */}
