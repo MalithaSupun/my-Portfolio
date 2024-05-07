9e8fe9d3df5e629e50af72921b4f03c020cdaa79
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-05-07 12:35:32 +0530

    update

--- src/Components/Getintouch.jsx
+++ src/Components/Getintouch.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import mailicon from './../Assets/get-intouch/mail.png'
 import mailcopyicon from './../Assets/get-intouch/copy.png'
 import telephoneicon from './../Assets/get-intouch/telephone.png'
@@ -8,6 +8,28 @@ import facebookicon from './../Assets/get-intouch/facebook-app-symbol.png'
 import linkedinicon from './../Assets/get-intouch/linkedin.png'
 
 const Getintouch = () => {
+  useEffect(() => {
+    const image = document.getElementById('mobileicong-i-t');
+    image.classList.add('shake');
+    return () => {
+      image.classList.remove('shake');
+    };
+  }, []);
+
+  const copyTextToClipboard = (text) => {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        console.log('Text copied to clipboard:', text);
+        // You can optionally add a success message or feedback here
+      })
+      .catch(err => {
+        console.error('Failed to copy text to clipboard:', err);
+        // Handle any errors here
+      });
+  };
+
+  
+  
   return (
     <>
     <div className="getintouchcontainer">
@@ -18,12 +40,12 @@ const Getintouch = () => {
         <div className="getintouchmail">
             <img id='mailicong-i-t' src={mailicon} alt=""/>
             <h2 id='get-in-t-h2'>malithasupun2@gmail.com</h2>
-            <img id='mailicong-i-tcopy' src={mailcopyicon} alt=""/>
+            <button><img id='mailicong-i-tcopy' src={mailcopyicon} alt=""  onClick={() => copyTextToClipboard('malithasupun2@gmail.com')}/></button>
         </div>
         <div className="getintouchmobile">
-            <img id='mobileicong-i-t' src={telephoneicon} alt=""/>
+        <img id='mobileicong-i-t' src={telephoneicon} alt=""/>
             <h2 id='get-in-t-h2'>+94 718465170</h2>
-            <img id='mobileicong-i-tcopy' src={mailcopyicon} alt=""/>
+            <button><img id='mobileicong-i-tcopy' src={mailcopyicon} alt="" onClick={() => copyTextToClipboard('+94 718465170')}/></button>
         </div>
         <div className="get-in-touch-social-icons-section">
             <h6 id='get-i-t-h6-tag'>You may also find me on these platforms!</h6>
